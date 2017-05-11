05cfa26126774de5d57d242bc87ff0a7c0f40bb1
Amy Venturino <aventuri@umich.edu>
2017-05-11 14:45:09 -0500

    Added styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,22 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('HighFiving', function ( HighFive ){
+myApp.controller('HighFiving', function(HighFive) {
   console.log('NG');
-
-  function randomNum(max, min) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
 
   var vm = this;
 
-  vm.newHighFive = function (){
+  vm.newHighFive = function() {
     vm.count = HighFive.counterGetter();
-    vm.skill = randomNum(1,10);
+    vm.skill = HighFive.randomNum(1, 10);
     console.log(vm.skill);
-  };
+  }; //end new high five function
 
-  vm.highFive = function (skill) {
+  vm.highFive = function(skill) {
     var success = HighFive.highFive(skill);
-      if ( success ) {
-       HighFive.counterSetter();
-     }
+    if (success) {
+      HighFive.counterSetter();
+    }
     vm.count = HighFive.counterGetter();
-  };
+  }; //end high five function
 
-});//end controller
+}); //end controller
--- server/public/scripts/highFive-service.js
+++ server/public/scripts/highFive-service.js
@@ -1,26 +1,28 @@
-myApp.service('HighFive', function(){
+myApp.service('HighFive', function() {
 
   var counter = 0;
 
-  this.counterGetter = function(){
-  return counter;
+  this.counterGetter = function() {
+    return counter;
   };
 
-  this.counterSetter = function(){
-   console.log("counter setter running");
-   counter++;
- };
+  this.counterSetter = function() {
+    console.log("counter setter running");
+    counter++;
+  };
 
-this.highFive = function (skill){
-  var randomNum = function (max, min) {
+  this.randomNum = function(max, min) {
     return Math.floor(Math.random() * (max - min)) + min;
   };
-  var quality = randomNum(1, 10);
-  if  (quality >= skill ) {
-    return true;
-  }else{
-    return false;
-  }
-};//end highFive
 
-});//end get
+
+  this.highFive = function(skill) {
+    var quality = this.randomNum(1, 10);
+    if (quality >= skill) {
+      return true;
+    } else {
+      return false;
+    } //end else
+  }; //end highFive
+
+}); //end get
